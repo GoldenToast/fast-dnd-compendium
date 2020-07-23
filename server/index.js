5f3495846616780fe56d6e64b1d46cba7a9e3ea3
Sascha Kowark <saschakowark@googlemail.com>
2020-07-23 11:19:54 +0200

    init

--- server/index.js
+++ server/index.js
@@ -3,6 +3,11 @@ var app = require('express')();
 app.use('/', proxy('www.dnd5eapi.co'));
 const port = process.env.PORT || 3000
 
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(__dirname + '/public'))
+    app.get(/.*/), (req,res) => res.sendFile(__dirname + '/public/index.html')
+}
+
 app.listen(port, function () {
-    console.log('Example app listening on port ${port} !');
+    console.log(`Example app listening on port ${port} !`);
 });
